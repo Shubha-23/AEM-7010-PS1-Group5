5c783b4b6b4fcf51fdf67dad7cd78f29db74b4e0
luciawxr <xw427@cornell.edu>
2023-12-02 22:13:43 -0500

    update q2

--- Codes/PS3_Q2.R
+++ Codes/PS3_Q2.R
@@ -33,25 +33,27 @@ model2 <- lm(u_ijt ~ price + prom + factor(brand), data = data_reg)
 # Model 3
 model3 <-  lm(u_ijt ~ price + prom + factor(store*brand), data = data_reg)
 
-msummary(list("(i)"=model1, "(ii)"=model2,"(iii)"=model3), coef_omit = "factor", gof_omit = ".*")
+mdlatoc <- msummary(list("(i)"=model1, "(ii)"=model2,"(iii)"=model3), coef_omit = "factor", gof_omit = ".*",stars = TRUE,output = "Output/model_summary1.tex")
 
 # part (d),(e),(f)
-data_reg <- data_reg %>%
-  group_by(brand) %>%
-  mutate(hausman = (sum(price) - price) / (n()-1))
-
 
 # 2(d)
 model_4 = ivreg(u_ijt ~ price + prom | cost + prom, data = data_reg)
 model_5 = ivreg(u_ijt ~ price + prom + factor(brand) | cost  + prom + factor(brand), data = data_reg)
 model_6 = ivreg(u_ijt ~ price + prom + factor(store*brand) | cost  + prom + factor(store*brand), data = data_reg)
 
-msummary(list("(iv)"=model_4, "(v)"=model_5,"(vi)"=model_6), coef_omit = "factor|Intercept", gof_omit = ".*")
+mdld <- msummary(list("(iv)"=model_4, "(v)"=model_5,"(vi)"=model_6), coef_omit = "factor|Intercept", gof_omit = ".*",stars = TRUE,output = "Output/model_summary2.tex")
+
 
 # 2(e)
+data_reg <- data_reg %>%
+  group_by(brand) %>%
+  mutate(hausman = (sum(price) - price) / (n()-1))
+
+
 model_7 = ivreg(u_ijt ~ price + prom | hausman + prom, data = data_reg)
 model_8 = ivreg(u_ijt ~ price + prom + factor(brand) | hausman + prom + factor(brand), data = data_reg)
 model_9 = ivreg(u_ijt ~ price + prom + factor(store*brand) | hausman + prom + factor(store*brand), data = data_reg)
 
-msummary(list("(vii)"=model_7, "(viii)"=model_8, "(ix)"=model_9), coef_omit = "factor|Intercept", gof_omit = ".*")
+mdlde <- msummary(list("(vii)"=model_7, "(viii)"=model_8, "(ix)"=model_9), coef_omit = "factor|Intercept", gof_omit = ".*",stars = TRUE,output = "Output/model_summary3.tex")
 
